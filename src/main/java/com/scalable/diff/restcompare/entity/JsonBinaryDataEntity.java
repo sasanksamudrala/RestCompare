78aace44f60334669fd38bae381881ef1f412c63
Sasank <Sasank.samudrala@gmail.com>
2017-07-07 12:10:07 +0200

    code commit for Lob

--- src/main/java/com/scalable/diff/restcompare/entity/JsonBinaryDataEntity.java
+++ src/main/java/com/scalable/diff/restcompare/entity/JsonBinaryDataEntity.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -27,6 +28,7 @@ public class JsonBinaryDataEntity {
     private Integer inputId;
     
     @NotNull
+    @Lob
     private String encodedValue;
     
     @NotNull
